57630f8e442bc0a017999995da3a26cbbe5fbafe
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-03-21 00:56:56 -0700

    update:changes makeit smart label

--- src/components/makeitsmart/AssesmentOne.jsx
+++ src/components/makeitsmart/AssesmentOne.jsx
@@ -22,110 +22,110 @@ const AssessmentOne = ({
   });
 
   return (
-    <div>
-      <Formik
-        initialValues={{
-          specific: inputData.main_goal_name || formData.specific || "",
-          specificedit: formData.specificedit || "",
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values) => {
-          setFormData({
-            ...formData,
-            specific: values.specific,
-            specificedit: values.specificedit,
-          });
-          nextStep();
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form>
-            <div className="mb-4">
-              <label
-                className="block text-sm font-medium mb-2"
-                htmlFor="specific"
-              >
-                Specific
-              </label>
-              <p className=" text-[14px] text-[#000000] font-[400] w-[503px] ">
-                When a goal is specific, it's like drawing a clear picture in
-                your mind of what you want to do. You can make your goal
-                specific by answering the following questions:
-              </p>
-              <div className="flex items-center gap-4 mt-3 mb-3">
-                {[
-                  "What do I want to do?",
-                  "Why do I want to do it?",
-                  "How will I do it?",
-                ]?.map((item, index) => (
-                  <div key={index} className="flex items-center">
-                    <img
-                      src={Stars}
-                      className="w-[15.89px] h-[12.93px] "
-                      alt=""
-                    />
-                    <span className="text-[14px] leading-[18.9px] text-[#012C57] font-[500] ">
-                      {item}
-                    </span>
-                  </div>
-                ))}
-              </div>
-              <Field
-                as="input"
-                id="specific"
-                name="specific"
-                placeholder="Describe Here"
-                className={`border  border-gray-400 rounded-lg w-full py-2 px-3 placeholder-gray-900 text-sm
-                   bg-transparent text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                     errors.specific && touched.specific ? "border-red-500" : ""
-                   }`}
-                maxLength="250"
-                disabled
-              />
-              <ErrorMessage
-                name="specific"
-                component="div"
-                className="text-red-500 text-xs italic"
-              />
-              <div className="mt-3">
-              <label
-                className="block text-sm font-medium mb-2"
-                htmlFor="specific"
-              >
-               Make it Specific
-              </label>
-                <Field
-                  as="input"
-                  id="specificedit"
-                  name="specificedit"
-                  placeholder="Describe Here"
-                  className={`border border-gray-400 rounded-lg w-full py-2 px-3 placeholder-gray-900 text-sm
-                   bg-transparent text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                     errors.specificedit && touched.specificedit
-                       ? "border-red-500"
-                       : ""
-                   }`}
-                  maxLength="250"
+    <Formik
+    initialValues={{
+      specific: inputData.main_goal_name || formData.specific || "",
+      specificedit: formData.specificedit || "",
+    }}
+    validationSchema={validationSchema}
+    onSubmit={(values) => {
+      setFormData({
+        ...formData,
+        specific: values.specific,
+        specificedit: values.specificedit,
+      });
+      nextStep();
+    }}
+  >
+    {({ values, errors, touched, setFieldValue }) => (
+      <Form>
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-2" htmlFor="specific">
+            Specific
+          </label>
+          <p className="text-[14px] text-[#000000] font-[400] w-[503px]">
+            When a goal is specific, it's like drawing a clear picture in
+            your mind of what you want to do. You can make your goal
+            specific by answering the following questions:
+          </p>
+          <div className="flex items-center gap-4 mt-3 mb-3">
+            {[
+              "What do I want to do?",
+              "Why do I want to do it?",
+              "How will I do it?",
+            ].map((item, index) => (
+              <div key={index} className="flex items-center">
+                <img
+                  src={Stars}
+                  className="w-[15.89px] h-[12.93px]"
+                  alt=""
                 />
-                <ErrorMessage
-                  name="specificedit"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
+                <span className="text-[14px] leading-[18.9px] text-[#012C57] font-[500]">
+                  {item}
+                </span>
               </div>
-            </div>
-            <div className="flex justify-center pt-4">
-              <div className="w-[343px]">
-                <AuthSubmitBtn text={"Next"} type={"submit"} />
-              </div>
-            </div>
-            <div className="flex justify-center mt-4">
-              <Backbutton />
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
+            ))}
+          </div>
+          <Field
+            as="input"
+            id="specific"
+            name="specific"
+            placeholder="Describe Here"
+            className={`border border-gray-400 rounded-lg w-full py-2 px-3 placeholder-gray-900 text-sm
+               bg-transparent text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                 errors.specific && touched.specific ? "border-red-500" : ""
+               }`}
+            maxLength="250"
+            disabled={!!inputData.main_goal_name} // Disable if main_goal_name exists
+            onChange={(e) => {
+              const value = e.target.value;
+              setFieldValue("specific", value);
+              if (!inputData.main_goal_name) {
+                setFieldValue("specificedit", value);
+              }
+            }}
+          />
+          <ErrorMessage
+            name="specific"
+            component="div"
+            className="text-red-500 text-xs italic"
+          />
+          <div className="mt-3">
+            <label className="block text-sm font-medium mb-2" htmlFor="specificedit">
+              Make it Specific
+            </label>
+            <Field
+              as="input"
+              id="specificedit"
+              name="specificedit"
+              placeholder="Describe Here"
+              className={`border border-gray-400 rounded-lg w-full py-2 px-3 placeholder-gray-900 text-sm
+               bg-transparent text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                 errors.specificedit && touched.specificedit
+                   ? "border-red-500"
+                   : ""
+               }`}
+              maxLength="250"
+            />
+            <ErrorMessage
+              name="specificedit"
+              component="div"
+              className="text-red-500 text-xs italic"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center pt-4">
+          <div className="w-[343px]">
+            <AuthSubmitBtn text={"Next"} type={"submit"} />
+          </div>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Backbutton />
+        </div>
+      </Form>
+    )}
+  </Formik>
+  
   );
 };
 
